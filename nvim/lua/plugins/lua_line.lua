0eab237ad390b26e76408fada065ef64b6ff61a5
JordenHuang <jorden90573@gmail.com>
2026-03-31 16:06:20 +0800

    Update neovim config
    - Make lualine looks minimal
    - Tweak whick-key

--- nvim/lua/plugins/lua_line.lua
+++ nvim/lua/plugins/lua_line.lua
@@ -1,46 +1,78 @@
+-- local function show_time()
+--     return " " .. os.date("%H:%M")
+-- end
+--
+-- local old_setup = {
+--     options = {
+--         icons_enabled = true,
+--         -- theme = "catppuccin",
+--         -- component_separators = { left = '', right = '' },
+--         -- section_separators = { left = '', right = '' },
+--         component_separators = { left = '', right = '' },
+--         section_separators = { left = '', right = '' },
+--         globalstatus = true, --(vim.o.laststatus == 3),
+--     },
+--     sections = {
+--         lualine_a = {
+--             {
+--                 'mode',
+--                 separator = { right = ''}
+--             },
+--         },
+--         lualine_b = {
+--             {
+--                 'windows',
+--                 section_separators = { left = '', right = '' },
+--                 component_separators = { left = '|', right = '' },
+--                 filetype_names = { oil = 'Oil' }, 
+--             },
+--         },
+--         lualine_c = {
+--             {
+--                 'branch',
+--                 color = {fg='LightBlue'},
+--             },
+--             'diff', 'diagnostics'
+--         },
+--         lualine_x = {'encoding', 'filetype'},
+--         lualine_y = {'progress'},
+--         lualine_z = {'location'}
+--     }
+-- }
+
 local lua_line = {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        local function show_time()
-            return " " .. os.date("%H:%M")
-        end
-
         require("lualine").setup{
             options = {
-                icons_enabled = true,
-                -- theme = "catppuccin",
-                -- component_separators = { left = '', right = '' },
-                -- section_separators = { left = '', right = '' },
+                icons_enabled = false,
                 component_separators = { left = '', right = '' },
                 section_separators = { left = '', right = '' },
-                globalstatus = true, --(vim.o.laststatus == 3),
             },
             sections = {
-                lualine_a = {
-                    {
-                        'mode',
-                        separator = { right = ''}
-                    },
-                },
-                lualine_b = {
-                    {
-                        'windows',
-                        section_separators = { left = '', right = '' },
-                        component_separators = { left = '|', right = '' },
-                        filetype_names = { oil = 'Oil' }, 
-                    },
-                },
+                lualine_a = {},
+                lualine_b = {},
                 lualine_c = {
+                    {
+                        'filename',
+                        color = {gui='bold'},
+                    },
                     {
                         'branch',
                         color = {fg='LightBlue'},
                     },
-                    'diff', 'diagnostics'
+                    'diff',
+                    'diagnostics',
                 },
-                lualine_x = {'encoding', 'filetype'},
-                lualine_y = {'progress'},
-                lualine_z = {'location'}
+                lualine_x = {
+                    'encoding',
+                    'filetype',
+                    'location',
+                    'progress',
+                },
+                lualine_y = {},
+                lualine_z = {}
             }
         }
     end
--- nvim/lua/plugins/which_key.lua
+++ nvim/lua/plugins/which_key.lua
@@ -1,22 +1,49 @@
 local which_key = {
     "folke/which-key.nvim",
-    tag = "v2.1.0",
+    tag = "v3.17.0",
     event = "VeryLazy",
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 1750
-    end,
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
-    }
+        preset = "helix",
+        delay = function(ctx)
+            return ctx.plugin and 0 or 1750
+        end,
+        icons = {
+            keys = {
+                Up = " ",
+                Down = " ",
+                Left = " ",
+                Right = " ",
+                C = "C- ",
+                M = "M- ",
+                D = "󰘳 ",
+                S = "S- ",
+                CR = "<CR> ",
+                Esc = "󱊷 ",
+                ScrollWheelDown = "󱕐 ",
+                ScrollWheelUp = "󱕑 ",
+                NL = "<CR> ",
+                BS = "󰁮",
+                Space = "SPACE ",
+                Tab = "󰌒 ",
+                F1 = "󱊫",
+                F2 = "󱊬",
+                F3 = "󱊭",
+                F4 = "󱊮",
+                F5 = "󱊯",
+                F6 = "󱊰",
+                F7 = "󱊱",
+                F8 = "󱊲",
+                F9 = "󱊳",
+                F10 = "󱊴",
+                F11 = "󱊵",
+                F12 = "󱊶",
+            },
+        }
+    },
 }
-
-
-
-
-
 
 return {
     which_key
